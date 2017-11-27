75c019eb4354d91d358f20cef37faa0920383cfe
klba <kamil.bak@gft.com>
2017-11-27 17:50:12 +0100

    fix some style, added some th

--- app/states/search/search.controller.js
+++ app/states/search/search.controller.js
@@ -1,19 +1,41 @@
 export class SearchController {
-    dataList = [];
+
+    dataList = []; 
+    pages = [];
+
     constructor (DataService) {
         'ngInject';
 
-        DataService.getData().then(searchData => {
-         
-            try {
-                console.log(searchData);
+        DataService.getData()
+            .then(searchData => {
                 this.dataList = searchData.data;
-            }
-            catch(err) {
-                console.log(err.message);
-            }
 
+                const pageSize = 10;
+                let currentPage = 1;
+                let numPages = Math.ceil(this.dataList.length / pageSize);
+
+                // slice data per current page
+                this.dataList = this.sliceDataToShow(currentPage, pageSize);
+
+                // create an array of pages to ng-repeat, + 1 for a correct number
+                this.pages = [...Array(numPages + 1).keys()].slice(1, numPages + 1);
+
+                console.log(this.pages);
+
+              
+                    
+        }).catch(err =>{
+            console.log(err)
         });
 
+    }
+
+    sliceDataToShow(currentPage, pageSize) {
+        let startIndex = (currentPage - 1) * pageSize;
+        let endIndex = startIndex + pageSize;
+
+        let dataVisiblePage = this.dataList.slice(startIndex, endIndex);
+
+        return dataVisiblePage;
     }
 }
