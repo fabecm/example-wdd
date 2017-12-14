db88864e3f8eccbd5c17f0ad9d8bb513fbc874e8
Fabio Bellini <fabio.bellini@crispybacon.it>
2017-12-14 19:46:26 +0100

    Implemented Data dots

--- app/states/dataDetail/dataDetail.controller.js
+++ app/states/dataDetail/dataDetail.controller.js
@@ -2,6 +2,26 @@ export class DataDetailController {
 
     listDataDetails = [];
 
+    processes = [{
+        label: 'Step 1',
+        enabled: true
+    }, {
+        label: 'Step 2',
+        enabled: true
+    }, {
+        label: 'Step 3',
+        enabled: true
+    }, {
+        label: 'Step 4',
+        enabled: true
+    }, {
+        label: 'Step 5',
+        enabled: false
+    }, {
+        label: 'Step 6',
+        enabled: false
+    }]
+
     constructor (DetailsService, $stateParams) {
         'ngInject';
         this.detailsService = DetailsService;
