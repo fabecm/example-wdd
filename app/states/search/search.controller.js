53de0679e697fe712897792fd31e122e5eb1c10c
Davide Uzzo <davide.uzzo@gft.com>
2017-12-15 15:59:34 +0100

    Fix wddDatepicker, added tabulation in search page and other fixes

--- app/states/search/search.controller.js
+++ app/states/search/search.controller.js
@@ -14,6 +14,9 @@ export class SearchController {
         label: 'Produzione'
     }];
 
+    selectedTab = 0;
+    // 0 is Data, 1 is Entity
+
     tablePagination = true;
     tablePageSize = 10;
 
@@ -55,6 +58,23 @@ export class SearchController {
         value: 'process_owner'
     }];
 
+    headerEntityTable = [{
+        label: 'Term Type',
+        value: 'term_type'
+    }, {
+        label: 'Term Name',
+        value: 'term_name'
+    }, {
+        label: 'Description',
+        value: 'description'
+    }, {
+        label: 'Stato',
+        value: 'state'
+    }, {
+        label: 'Ultima Modifica',
+        value: 'last_change'
+    }];
+
     constructor (DataService, DatasourceService) {
         'ngInject';
         this.datasourceService = DatasourceService;
@@ -79,30 +99,8 @@ export class SearchController {
         });
     }
 
-    sliceDataToShow (currentPage, pageSize) {
-        let startIndex = (Number(currentPage) - 1) * Number(pageSize);
-        let endIndex = startIndex + pageSize;
-        let dataVisiblePage = this.rawData.slice(startIndex, endIndex);
-
-        return dataVisiblePage;
-    }
-
-    changingPage (obj) {
-        this.currentPage = obj;
-        this.dataList = this.rawData;
-        this.dataList = this.sliceDataToShow(this.currentPage, this.pageSize);
-    }
-
-    changingPageToFirst () {
-        this.currentPage = 1;
-        this.dataList = this.rawData;
-        this.dataList = this.sliceDataToShow(this.currentPage, this.pageSize);
-    }
-
-    changingPageToLast () {
-        this.currentPage = this.pages.length;
-        this.dataList = this.rawData;
-        this.dataList = this.sliceDataToShow(this.currentPage, this.pageSize);
+    changeTab(tab) {
+        this.selectedTab = tab;
     }
 
     filterChanged (arrayFilter) {
@@ -124,12 +122,14 @@ export class SearchController {
         this.dataService.getData(param)
         .then(searchData => {
             this.rawData = angular.copy(searchData.data.OutputArray);
-            this.dataList = searchData.data.OutputArray;
-            let numPages = Math.ceil(this.dataList.length / this.pageSize);
+            // this.dataList = searchData.data.OutputArray;
+            let tableEntity = getEntityTable();
+            this.rawDataEntity = tableEntity.data.OutputArray;
+            // let numPages = Math.ceil(this.dataList.length / this.pageSize);
 
-            this.dataList = this.sliceDataToShow(this.currentPage, this.pageSize);
+            // this.dataList = this.sliceDataToShow(this.currentPage, this.pageSize);
 
-            this.pages = [...Array(numPages + 1).keys()].slice(1, numPages + 1);
+            // this.pages = [...Array(numPages + 1).keys()].slice(1, numPages + 1);
         });
     }
 
@@ -140,5 +140,123 @@ export class SearchController {
                 systemOwner: res.data.system_owner
             };
         });
+    }
+}
+
+function getEntityTable () {
+    return {
+        data: {
+            OutputArray: [
+                {
+                    term_type: {
+                        label: 'Term type 1',
+                        value: 1
+                    },
+                    term_name: {
+                        label: 'Term name 1',
+                        value: 1
+                    },
+                    description: {
+                        label: 'Description 1',
+                        value: 1
+                    },
+                    state: {
+                        label: 'Stato 1',
+                        value: 1
+                    },
+                    last_change: {
+                        label: '30/06/2006',
+                        value: 1
+                    }
+                },
+                {
+                    term_type: {
+                        label: 'Term type 2',
+                        value: 2
+                    },
+                    term_name: {
+                        label: 'Term name 2',
+                        value: 2
+                    },
+                    description: {
+                        label: 'Description 2',
+                        value: 2
+                    },
+                    state: {
+                        label: 'Stato 2',
+                        value: 2
+                    },
+                    last_change: {
+                        label: '30/06/2006',
+                        value: 2
+                    }
+                },
+                {
+                    term_type: {
+                        label: 'Term type 3',
+                        value: 3
+                    },
+                    term_name: {
+                        label: 'Term name 3',
+                        value: 3
+                    },
+                    description: {
+                        label: 'Description 3',
+                        value: 3
+                    },
+                    state: {
+                        label: 'Stato 3',
+                        value: 3
+                    },
+                    last_change: {
+                        label: '30/06/2006',
+                        value: 3
+                    }
+                }, {
+                    term_type: {
+                        label: 'Term type 4',
+                        value: 4
+                    },
+                    term_name: {
+                        label: 'Term name 4',
+                        value: 4
+                    },
+                    description: {
+                        label: 'Description 4',
+                        value: 4
+                    },
+                    state: {
+                        label: 'Stato 4',
+                        value: 4
+                    },
+                    last_change: {
+                        label: '30/06/2006',
+                        value: 4
+                    }
+                },
+                {
+                    term_type: {
+                        label: 'Term type 5',
+                        value: 5
+                    },
+                    term_name: {
+                        label: 'Term name 5',
+                        value: 5
+                    },
+                    description: {
+                        label: 'Description 5',
+                        value: 5
+                    },
+                    state: {
+                        label: 'Stato 5',
+                        value: 5
+                    },
+                    last_change: {
+                        label: '30/06/2006',
+                        value: 5
+                    }
+                }
+            ]
+        }
     }
 }
