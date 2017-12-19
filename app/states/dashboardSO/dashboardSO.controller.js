b47a7fe243586d70ee4e9408f8c041ccaa1b6509
Davide Uzzo <davide.uzzo@gft.com>
2017-12-19 18:19:49 +0100

    action tooltip, fix filter, table strip and other fix

--- app/states/dashboardSO/dashboardSO.controller.js
+++ app/states/dashboardSO/dashboardSO.controller.js
@@ -10,7 +10,7 @@ export class DashboardSOController {
     }, {
         label: 'Censimenti di iniziativa',
         value: 0,
-        color: '#F2BE00',
+        color: '#00796B',
         childPage: 'tab.initiativeCensuses'
     }];
 
