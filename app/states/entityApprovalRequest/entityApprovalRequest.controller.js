50c6544f63f350df58a33d77b01b2426205501c8
Davide Uzzo <davide.uzzo@gft.com>
2018-02-03 15:45:05 +0100

    Progress entity approval request

--- app/states/entityApprovalRequest/entityApprovalRequest.controller.js
+++ app/states/entityApprovalRequest/entityApprovalRequest.controller.js
@@ -1,9 +1,25 @@
 export class EntityApprovalRequestController {
 
-
     tablePagination = true;
     tablePageSize = 10;
     tableExpandable = true;
+
+    // headerTable = [{
+    //     label: 'Workspace',
+    //     value: 'workspace'
+    // }, {
+    //     label: 'Descrizione',
+    //     value: 'description'
+    // }, {
+    //     label: 'Data Inizio',
+    //     value: 'start_date'
+    // }, {
+    //     label: 'Data Fine',
+    //     value: 'end_date'
+    // }, {
+    //     label: 'Stato',
+    //     value: 'status'
+    // }];
 
     constructor (SearchWorkspaceService, $q, $state, ModalService) {
         'ngInject';
@@ -12,19 +28,11 @@ export class EntityApprovalRequestController {
         this.$state = $state;
         this.modalService = ModalService;
 
-        this.initRuDashboard();
+        this.initEntityApprovalRequest();
     }
 
-    initRuDashboard () {
-        this.searchWorkspaceService.getWorkspaceRu().then(res => {
-            this.workspaceData = res.outputArray;
-            getHeader(this.$q).then(headerRes => {
-                this.headerTable = headerRes;
-            });
-            getHeaderExpandable(this.$q).then(headerExpRes => {
-                this.headerTableExpandable = headerExpRes;
-            });
-        });
+    initEntityApprovalRequest () {
+
     }
 
     changeChild () {
@@ -33,46 +41,4 @@ export class EntityApprovalRequestController {
         }
     }
 
-    createNewWorkspace () {
-        this.modalService.openNewWorkspaceModal();
-    }
-}
-
-function getHeader ($q) {
-    let defer = $q.defer();
-    defer.resolve([{
-        label: 'Tipo entità',
-        value: 'workspace'
-    }, {
-        label: 'Nome entità',
-        value: 'description'
-    }, {
-        label: 'Descrizione',
-        value: 'start_date'
-    }, {
-        label: 'Stato',
-        value: 'state'
-    }]);
-    return defer.promise;
-}
-
-function getHeaderExpandable ($q) {
-    let defer = $q.defer();
-    defer.resolve([{
-        label: 'Data Field',
-        value: 'data_field'
-    }, {
-        label: 'Data source table',
-        value: 'data_table'
-    }, {
-        label: 'Data source',
-        value: 'data_source'
-    }, {
-        label: 'Technical application',
-        value: 'data_source'
-    }, {
-        label: 'System Owner',
-        value: 'data_source'
-    }]);
-    return defer.promise;
 }
