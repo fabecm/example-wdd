d3fc4876fb4f9488f580fd00faad8db953dfce4a
Unknown <erika.gili@icloud.com>
2018-04-16 17:43:12 +0200

    Applying the sortable to some tables and api calls

--- app/core/resources/services/data.js
+++ app/core/resources/services/data.js
@@ -34,6 +34,13 @@ export class DataService {
     }
 
     getDQEntityApprovalRequest (pageNumber, pageLength, params = {}) {
+        if (params.order_by && params.order_type) {
+            let oBy = encodeURIComponent(params.order_by);
+            let oType = encodeURIComponent(params.order_type);
+            delete params.order_by;
+            delete params.order_type;
+            return this.$http.post(`WDD/search/dataquality/entityapproval?pageNumber=${pageNumber}&pageLength=${pageLength}&order_by=${oBy}&order_type=${oType}`, params);
+        }
         return this.$http.post(`WDD/search/dataquality/entityapproval?pageNumber=${pageNumber}&pageLength=${pageLength}`, params);
     }
 
